bbcb32fd175b9c355e65bcc7ab43d7d1370c0ef7
김용순 <dydtns9815@gmail.com>
2025-01-16 17:02:47 +0900

    feat: migrate ESLint configuration from .eslintrc.json to eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.push({
+  rules: {
+    "react/no-unescaped-entities": "off",
+  },
+});
+
 export default eslintConfig;
