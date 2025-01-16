3e51710b59b0220230a8ab9b2c030087bac57240
김용순 <dydtns9815@gmail.com>
2025-01-16 20:11:59 +0900

    feat: add main image to Home page for enhanced visual appeal

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
     <div className="flex-grow flex flex-col justify-center items-center">
+      <img
+        src="/main-img.png"
+        alt="main image"
+        className="max-h-80 shadow-lg border"
+      />
       <h2 className="text-5xl font-semibold">Welcome</h2>
       <p className="mt-4 text-gray-700 dark:">I&apos;m soongle</p>
     </div>
