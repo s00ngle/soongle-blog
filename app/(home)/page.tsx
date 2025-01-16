1110a5885665ec03b752c7b244f05684559ee4f7
김용순 <dydtns9815@gmail.com>
2025-01-16 20:17:51 +0900

    feat: update Home page styling with background color and image adjustments for better aesthetics

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div className="flex-grow flex flex-col justify-center items-center">
+    <div className="flex-grow flex flex-col justify-center items-center bg-gray-50">
       <img
         src="/main-img.png"
         alt="main image"
-        className="max-h-80 shadow-lg border border-gray-50 hover:scale-105 transition-transform duration-300"
+        className="h-72 w-72 rounded-full shadow-lg border border-gray-50 hover:scale-105 transition-transform duration-300 bg-blue-100"
       />
       <h2 className="text-5xl font-semibold mt-4">Welcome</h2>
       <p className="mt-4 text-gray-700 dark:">I&apos;m soongle</p>
