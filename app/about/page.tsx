0bb04a62fe999bfecaa3bae00d4cb7739e67ed5e
김용순 <dydtns9815@gmail.com>
2025-01-16 17:46:37 +0900

    feat: integrate social media buttons directly into About page and remove separate data file

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,32 @@
 import InfoSection from "@/components/InfoSection";
 import SocialMediaButton from "@/components/SocialMediaButton";
 import { Metadata } from "next";
-import SOCIAL_MEDIA_BUTTONS from "@/data/about/socialMediaButtons";
 import activities from "@/data/about/activities";
 import awards from "@/data/about/awards";
+import { FaGithub, FaInstagram, FaYoutube } from "react-icons/fa";
 
 export const metadata: Metadata = {
   title: "About",
 };
+
+const SOCIAL_MEDIA_BUTTONS = [
+  {
+    href: "https://www.youtube.com/@soongle",
+    colors: "bg-red-600 text-white",
+    icon: <FaYoutube className="text-3xl" />,
+  },
+  {
+    href: "https://www.instagram.com/kyso_on/",
+    colors:
+      "bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white",
+    icon: <FaInstagram className="text-3xl" />,
+  },
+  {
+    href: "https://github.com/s00ngle",
+    colors: "bg-gray-800 text-white",
+    icon: <FaGithub className="text-3xl" />,
+  },
+];
 
 const AboutPage = () => {
   return (
