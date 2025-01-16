182253ab79ed316609819935fe1d6b7c843dca99
김용순 <dydtns9815@gmail.com>
2025-01-16 20:14:41 +0900

    feat: enhance main image styling and adjust heading margin for improved layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
       <img
         src="/main-img.png"
         alt="main image"
-        className="max-h-80 shadow-lg border"
+        className="max-h-80 shadow-lg border border-gray-50 hover:scale-105 transition-transform duration-300"
       />
-      <h2 className="text-5xl font-semibold">Welcome</h2>
+      <h2 className="text-5xl font-semibold mt-4">Welcome</h2>
       <p className="mt-4 text-gray-700 dark:">I&apos;m soongle</p>
     </div>
   );
