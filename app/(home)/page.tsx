ac22ee1a6fd6e692677ddefbfcca651df42aab6d
김용순 <dydtns9815@gmail.com>
2025-01-17 11:19:11 +0900

    feat: replace img with Image component for optimized loading and improved performance

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex-grow flex flex-col justify-center items-center bg-gray-50">
-      <img
+      <Image
         src="/main-img.png"
         alt="main image"
+        width={200}
+        height={200}
         className="h-72 w-72 rounded-full shadow-lg border border-gray-50 hover:scale-105 transition-transform duration-300 bg-blue-100"
       />
       <h2 className="text-5xl font-semibold mt-4">Welcome</h2>
