1cc3ed793bef19ef49e83c6a721f11c9c8967592
김용순 <dydtns9815@gmail.com>
2025-02-26 17:43:23 +0900

    feat: add console logo output to Home component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,26 @@ import PageContainer from "@/components/PageContainer";
 import Image from "next/image";
 
 export default function Home() {
+  // 콘솔 로고 출력
+  console.log(
+    `
+    %c  ███████╗ ██████╗  ██████╗ ███╗   ██╗ ██████╗ ██╗     ███████╗
+    %c  ██╔════╝██╔═══██╗██╔═══██╗████╗  ██║██╔════╝ ██║     ██╔════╝
+    %c  ███████╗██║   ██║██║   ██║██╔██╗ ██║██║  ███╗██║     █████╗  
+    %c  ╚════██║██║   ██║██║   ██║██║╚██╗██║██║   ██║██║     ██╔══╝  
+    %c  ███████║╚██████╔╝╚██████╔╝██║ ╚████║╚██████╔╝███████╗███████╗
+    %c  ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚══════╝
+    %c  This is Soongle's space. Welcome! 🎸💻
+    `,
+    "color:#4387f4",
+    "color:#b477ef",
+    "color:#fb63c8",
+    "color:#ff7a95",
+    "color:#ff9368",
+    "color:#f5b400",
+    "color:#ffffff"
+  );
+
   return (
     <PageContainer>
       <Image
@@ -9,6 +29,7 @@ export default function Home() {
         alt="main image"
         width={500}
         height={500}
+        priority
         className="h-72 w-72 rounded-full shadow-lg border border-gray-50 hover:scale-105 transition-transform duration-300 bg-blue-100 object-cover"
       />
       <h2 className="text-5xl font-semibold mt-4">Welcome</h2>
